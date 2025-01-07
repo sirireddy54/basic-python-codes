a3c11733204a80b6d9978468d92fbce870cd9eaa
sirireddy54 <sirireddy229@gmail.com>
2025-01-07 12:56:57 -0600

    new file added

---
+++ Cargame.py
@@ -0,0 +1,17 @@
+
+
+
+while(0<1):  
+    command= str(input()).lower()
+    if(command=="help"):
+        print("start to start the car \n Stop to stop the car \n quit to quit the program")
+    elif(command=="start"):
+        print("Car Strated....Ready to go")
+    elif(command=="stop"):
+        print("Car Stopped")
+    elif(command=="quit"):
+        break
+    else:
+        print("I dont understand that")
+
+
